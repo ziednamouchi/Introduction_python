7bd71c478161075a67f84023065324687e3fcaef
Zied Namouchi <zyed.namouchy@gmail.com>
2016-10-02 13:46:05 +0200

    Create first_chapter.py

---
+++ first_chapter.py
@@ -0,0 +1,23 @@
+#chapter1
+#Variables, names, and Objects
+a=7
+print(a)
+type(a)
+type('abc')
+#Numbers
+print(a-3)
+a=95
+temp=a-5
+a=temp
+a-=3
+print(a)
+b=divmod(9,5)
+print(b)
+#precedences
+print(2+3*4)
+print(2+(3*4))
+#bases
+print(10)
+print(0b10)
+print(0o10)
+print(0x10)
