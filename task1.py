ba070b3b7bb0cfc037575b0a9fa4aa0f4b706f24
Zied Namouchi <zyed.namouchy@gmail.com>
2016-10-04 23:19:00 +0100

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,14 @@
+#calculate number of seconds in an hour
+print("number of seconds in an hour is: ",60*60,"s")
+#assignig the result in a variable
+seconds_per_hour=60*60
+print("number of seconds in an hour is: ",seconds_per_hour,"s")
+#number of seconds in a day
+print("number of seconds in a day is: ",seconds_per_hour*24,"s")
+#number of seconds in a day
+seconds_per_day=seconds_per_hour*24
+print("number of seconds in a day is: ",seconds_per_day,"s")
+#float division
+print(seconds_per_day/seconds_per_hour)
+#integer division
+print(seconds_per_day//seconds_per_hour)
