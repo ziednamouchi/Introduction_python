563788f88e37944335a326c2dca5a7c0caf5406b
Zied Namouchi <zyed.namouchy@gmail.com>
2016-10-22 01:01:32 +0100

    Create report.py

---
+++ report.py
@@ -0,0 +1,6 @@
+#report.py
+def get_description():#see the docstring below
+    """Return random weather, just like the pros"""
+    from random import choice
+    possibilities = ['rain','snow','sleet','fog','sun','who knows']
+    return choice(possibilities)
